c409c6c6294021992048b7136869077075b0ad27
nodadyoushutup <admin@nodadyoushutup.com>
2024-11-24 03:46:15 +0000

    0.0.2

--- script/cloud-init.sh
+++ script/cloud-init.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -eu
 
 source /tmp/logger.sh
-log_info "Checking if Cloud-Init has completed..."
+log_info "Checking if Cloud-Init has completed"
 while [ ! -f /var/lib/cloud/instance/boot-finished ]; do
-    log_info "Waiting for Cloud-Init to finish..."
+    log_info "Waiting for Cloud-Init to finish"
     sleep 1
 done
 log_info "Cloud-Init has completed successfully!"
